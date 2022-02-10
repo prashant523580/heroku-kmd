21f7f397d90fe1e54cca50db3e5d773dc79ddac4
prashant523580 <prashant.thapa62758@gmail.com>
2022-02-10 13:57:58 +0545

    stye designed

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -20,12 +20,10 @@ function updateIndex(currentIndex){
 carouselMain.addEventListener("mouseleave",() => pause = false);
 prevBtn.addEventListener("click",() =>{
 	updateIndex(slideIndex - 1)
-	console.log(slideIndex)
 });
 
 nextBtn.addEventListener("click",() => {
 		updateIndex(slideIndex +1 );
-		console.log(slideIndex)
 });
 
 function slideShows(){
@@ -35,41 +33,18 @@ function slideShows(){
 	 }
 	
  	slideIndex++;
- 	updateIndex(slideIndex-1);
- 	// carousel[updateIndex(slideIndex-1)].style.opacity = "1";
- 	if(slideIndex > carousel.length){slideIndex = 1}
-
- 	let carousel_interval = setInterval( () => {
- 			if(pause === true){
- 				slideShows();
- 			// updateIndex(slideIndex + 1);
- 			}
- 	},3000);
- 	return () => {
- 		if(carousel_interval){
- 			clearInterval(carousel_interval)
- 		}
- 	} 	
+ 		carousel[slideIndex].style.opacity = "1";
+ 		setTimeout(() => {
+ 			slideIndex++
+ 		},2000);
  }
 
 let prevscroll = window.scrollY;
 function scrollFunc(){
 	let currentscroll = window.scrollY;
 	let nav = document.getElementsByTagName("nav")[0];
-
-	// if(scrolly >= 50){
-	// 	nav.style.backgroundImage ="linear-gradient( #076a62, #076d79 , #076e82 )";
-	// 	nav.style.padding = "20px 20px";
-	// 	nav.style.transition = ".3s ease";
-	// 	nav.style.color = "white";
-
-	// }else if(scrolly <= 50){
-	// 	nav.style.background = "none";
-	// 		nav.style.padding = "10px 10px"
-	// }
 	if(prevscroll < currentscroll){
 		nav.style.top = "-50px";
-
 		nav.style.position ="relative";
 	}else{
 		nav.style.top = "0"
@@ -81,57 +56,9 @@ function scrollFunc(){
 	// console.log({prevscroll,currentscroll})
 }
  window.addEventListener("scroll", scrollFunc);
-window.addEventListener("load", () => {
+(window.addEventListener("load", () => {
 	if("serviceWorker" in navigator){
 		navigator.serviceWorker.register("/sw.js");
 	}
-})
+}))();
 
-// async function checkNetworkAndReload(){
-// 	try{
-// 		const res = await fetch(".");
-// 		// console.log(res)
-// 		if(res.status > 200 && res.status <500){
-// 			window.location.reload();
-// 			return
-// 		}
-// 	}catch(err){
-// 		console.log(err)
-// 	}
-// 	window.setTimeout(checkNetworkAndReload());
-// }
-
-// checkNetworkAndReload();
-
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-
-/* Put code here */
-
-
-
-/* Only register a service worker if it's supported */
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
-}
-
-/**
- * Warn the page must be served over HTTPS
- * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
- * Installability requires a service worker with a fetch event handler, and
- * if the page isn't served over HTTPS, the service worker won't load.
- */
-if (window.location.protocol === 'http:') {
-  const requireHTTPS = document.getElementById('requireHTTPS');
-  const link = requireHTTPS.querySelector('a');
-  link.href = window.location.href.replace('http://', 'https://');
-  requireHTTPS.classList.remove('hidden');
-}
-let deferredPrompt;
-window.addEventListener("beforeinstallprompt", (e) => {
-	e.preventDefault();
-	deferredPrompt = e;
-	showInstalledPromotion();
-	console.log("event was fire")
-
-})
